cb94ef4c48f0319be906f9719f1704946de8a1b5
Natsuhi Shimada <gumball7229@gmail.com>
2025-10-30 12:41:52 +0900

    add plus test

--- test.bash
+++ test.bash
@@ -1,12 +1,16 @@
-#!/bin/bash
+#!/bin/bash -xv
+# SPDX-FileCopyrightText: 2025 Natsuhi Shimada
+# SPDX-License-Identifier: BSD-3-Clause
 
-ng(){
-     echo ${1}行目が違うよ
-     res=1
+ng () {
+       echo ${1}行目が違うよ
+       res=1
 }
 
-a=島田
-[ "$a" = 山田 ] || ng "$LINENO"
-[ "$a" = 島田 ] || ng "$LINENO"
+res=0
 
+out=$(seq 5 | ./plus)
+[ "${out}" = 15 ] || ng "$LINENO"
+
+[ "${res}" = 0 ] && echo OK
 exit $res
